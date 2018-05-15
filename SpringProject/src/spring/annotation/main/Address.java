de4df6bd839c03d9e6850bf995bc88175248cc05
Ritesh Kumar <ritesh705@gmail.com>
2018-05-16 03:38:29 +0530

    Code formatted.

--- SpringProject/src/spring/annotation/main/Address.java
+++ SpringProject/src/spring/annotation/main/Address.java
@@ -1,28 +1,32 @@
 package spring.annotation.main;
 
-public class Address {
-
+public class Address
+{
 	private String city;
 	private String state;
 	private String country;
 	
-	public String getCity() {
+	public String getCity()
+	{
 		return city;
 	}
-	
-	public void setCity(String city) {
+	public void setCity(String city)
+	{
 		this.city = city;
 	}
-	public String getState() {
+	public String getState()
+	{
 		return state;
 	}
-	public void setState(String state) {
+	public void setState(String state)
+	{
 		this.state = state;
 	}
 	public String getCountry() {
 		return country;
 	}
-	public void setCountry(String country) {
+	public void setCountry(String country)
+	{
 		this.country = country;
 	}
 }
